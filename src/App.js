c865e678b1a1dcdc28c15ede9b048bb7059c5638
Shivani Aggarwal <shivanisa33@gmail.com>
2018-10-20 14:46:45 -0400

    sidebar background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import logo from './logo.svg'
 import './App.css'
+import SideBar from './components/SideBar/SideBar'
 import Earthquake from './screens/earthquake/earthquake'
 import VolcanicEruption from './screens/volcanicEruption/volcanicEruption'
 import Tornado from './screens/tornado/tornado'
@@ -11,33 +11,37 @@ class App extends Component {
   render() {
     const Home = () => <div>This is the home page</div>
     return (
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/earthquake">Earthquake</Link>
-            </li>
-            <li>
-              <Link to="/volcanicEruption">Volcanic Eruption</Link>
-            </li>
-            <li>
-              <Link to="/tornado">Tornado</Link>
-            </li>
-            <li>
-              <Link to="/tsunami">Tsunami</Link>
-            </li>
-          </ul>
-          <hr />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/earthquake" component={Earthquake} />
-          <Route exact path="/volcanicEruption" component={VolcanicEruption} />
-          <Route exact path="/tornado" component={Tornado} />
-          <Route exact path="/tsunami" component={Tsunami} />
-        </div>
-      </Router>
+      <div id="main">
+        <SideBar></SideBar>
+
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/earthquake">Earthquake</Link>
+              </li>
+              <li>
+                <Link to="/volcanicEruption">Volcanic Eruption</Link>
+              </li>
+              <li>
+                <Link to="/tornado">Tornado</Link>
+              </li>
+              <li>
+                <Link to="/tsunami">Tsunami</Link>
+              </li>
+            </ul>
+            <hr />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/earthquake" component={Earthquake} />
+            <Route exact path="/volcanicEruption" component={VolcanicEruption} />
+            <Route exact path="/tornado" component={Tornado} />
+            <Route exact path="/tsunami" component={Tsunami} />
+          </div>
+        </Router>
+      </div>
     )
   }
 }
---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import './SideBar.css'
+
+const SideBar = () => {
+	return (
+		<div id="sideBarBackground">
+			
+		</div>
+	);
+}
+
+export default SideBar;
