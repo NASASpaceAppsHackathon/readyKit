a98926c759660c9bf4701cafbdf275de3fe6a9ef
Hyeyeon Shin <hyeyeonshin@DF-586065.local>
2018-10-21 02:26:31 -0400

    description styling

--- client/src/components/description.js
+++ client/src/components/description.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import style from './description.module.css';
-import {Link} from 'react-router-dom';
+import React from 'react'
+import style from './description.module.css'
+import { Link } from 'react-router-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faArrowLeft);
+library.add(faArrowLeft)
 
-export const Description= (props) => (
-            
-            <div className={style.container}>
-            <Link to={props.link} className="button" style={{float: 'left'}}> <FontAwesomeIcon icon="arrow-left" size= '2x' color='#00134d'/></Link>
-                <h1 style={{textAlign: 'center'}}>{props.title}</h1>
-                <div className={style.descript} >
-                    <p>{props.description}</p>
-                </div>
-                <div className={style.descript} >
-                    <p>{props.description2}</p>
-                </div>
-                <div className={style.descript} >
-                    <p>{props.description3}</p>
-                </div>
-            </div>
-        )
+export const Description = props => (
+  <div className={style.container}>
+    <Link
+      to={props.link}
+      className="button"
+      style={{ position: 'absolute', top: '160px' }}
+    >
+      <FontAwesomeIcon icon="arrow-left" size="2x" color="#00134d" />
+    </Link>
+    <h1 className={style.title}>{props.title}</h1>
+
+    <div className={style.descript}>
+      <p>{props.description}</p>
+    </div>
+    <img alt="" src={props.img1} className={style.img} />
+    <div className={style.descript}>
+      <p>{props.description2}</p>
+    </div>
+    <img alt="" src={props.img2} className={style.img} />
+    <div className={style.descript}>
+      <p>{props.description3}</p>
+    </div>
+    <img alt="" src={props.img3} className={style.img} />
+  </div>
+)
