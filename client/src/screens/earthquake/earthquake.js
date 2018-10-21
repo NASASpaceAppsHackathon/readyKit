915f455ef532c54e049a8e796597830cfa64d8fc
Hyeyeon Shin <hyeyeonshin@DF-586065.local>
2018-10-21 11:23:57 -0400

    style changes

--- client/src/screens/earthquake/earthquake.js
+++ client/src/screens/earthquake/earthquake.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react'
 import { Description } from '../../components/description'
 import Checklist from '../../components/checklist'
-import img1 from '../../assets/earthquake-1.jpg';
-import img2 from '../../assets/earthquake-2.jpg';
+import img1 from '../../assets/earthquake-1.jpg'
+import img2 from '../../assets/earthquake-2.jpg'
 import style from '../../components/description.module.css'
 import { Link } from 'react-router-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faArrowLeft);
+library.add(faArrowLeft)
 
-const description="An earthquake is an intense shaking of Earth’s surface. The shaking is caused by movements in Earth’s outermost layer.";
-const description2="Although the Earth looks like a pretty solid place from the surface, it’s actually extremely active just below the surface. The Earth is made of four basic layers: a solid crust, a hot, nearly solid mantle, a liquid outer core and a solid inner core.The solid crust and top, stiff layer of the mantle make up a region called the lithosphere. The lithosphere isn’t a continuous piece that wraps around the whole Earth like an eggshell. It’s actually made up of giant puzzle pieces called tectonic plates. Tectonic plates are constantly shifting as they drift around on the viscous, or slowly flowing, mantle layer below."
-const description3= "This non-stop movement causes stress on Earth’s crust. When the stresses get too large, it leads to cracks called faults. When tectonic plates move, it also causes movements at the faults. An earthquake is the sudden movement of Earth’s crust at a fault line."
-const description4 = "If an earthquake happens, protect yourself right away. Drop, Cover, then Hold On!";
+const description =
+  'An earthquake is an intense shaking of Earth’s surface. The shaking is caused by movements in Earth’s outermost layer.'
+const description2 =
+  'Although the Earth looks like a pretty solid place from the surface, it’s actually extremely active just below the surface. The Earth is made of four basic layers: a solid crust, a hot, nearly solid mantle, a liquid outer core and a solid inner core.The solid crust and top, stiff layer of the mantle make up a region called the lithosphere. The lithosphere isn’t a continuous piece that wraps around the whole Earth like an eggshell. It’s actually made up of giant puzzle pieces called tectonic plates. Tectonic plates are constantly shifting as they drift around on the viscous, or slowly flowing, mantle layer below.'
+const description3 =
+  'This non-stop movement causes stress on Earth’s crust. When the stresses get too large, it leads to cracks called faults. When tectonic plates move, it also causes movements at the faults. An earthquake is the sudden movement of Earth’s crust at a fault line.'
+const description4 =
+  'If an earthquake happens, protect yourself right away. Drop, Cover, then Hold On!'
 
 class Earthquake extends Component {
   render() {
     return (
-        <div>
-         <div className={style.container}>
+      <div style={{ display: 'flex' }}>
+        <div className={style.container}>
           <Link
             to="/"
             className="button"
             style={{ position: 'absolute', top: '160px' }}
           >
-          <FontAwesomeIcon icon="arrow-left" size="2x" color="#00134d" />
+            <FontAwesomeIcon
+              style={{ marginTop: '-55px' }}
+              icon="arrow-left"
+              size="2x"
+              color="#00134d"
+            />
           </Link>
           <h1 className={style.title}>EARTHQUAKES</h1>
           <div className={style.descript}>
@@ -38,7 +47,7 @@ class Earthquake extends Component {
           </div>
           <img alt="" src={img2} className={style.img} />
           <div className={style.descript}>
-            <p></p>
+            <p />
           </div>
           <img alt="" className={style.img} />
           <div className={style.descript}>
@@ -49,21 +58,25 @@ class Earthquake extends Component {
             <p>{description4}</p>
             <p> Here is what we suggest:</p>
             <ul>
-              <li>Non-perishable food such as dehydrated food, powdered milk, canned juices, dried cereals, fruits and non-salted nuts</li>
+              <li>
+                Non-perishable food such as dehydrated food, powdered milk,
+                canned juices, dried cereals, fruits and non-salted nuts
+              </li>
               <li>Flashlights and spare batteries</li>
-              <li>Portable, battery-powered radio or television and spare batteries</li>
+              <li>
+                Portable, battery-powered radio or television and spare
+                batteries
+              </li>
               <li>First aid kit and manual</li>
               <li>Sturdy shoes</li>
               <li>Fire extinguishers</li>
             </ul>
           </div>
         </div>
-        <Checklist title="My Checklist"
-                  list_id="earthquake"
-                  />
+        <Checklist title="My Checklist" list_id="earthquake" />
       </div>
-      )
-    }
+    )
   }
+}
 
 export default Earthquake
--- client/src/screens/tornado/tornado.js
+++ client/src/screens/tornado/tornado.js
@@ -8,34 +8,41 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import style from '../../components/description.module.css'
 import img1 from '../../assets/tornado1.jpg'
 
+library.add(faArrowLeft)
 
-library.add(faArrowLeft);
-
-const description="Tornadoes are funnels of rapidly spinning air. Their winds may top 250 miles (400 kilometers) an hour and can clear a pathway a mile (1.6 kilometers) wide and 50 miles (80 kilometers) long.Twisters are born in thunderstorms and are often accompanied by hail. Giant, persistent thunderstorms called supercells spawn the most destructive tornadoes. These violent storms occur around the world, but the United States is a major hotspot with about a thousand tornadoes every year."
-const description2= "When there are thunderstorms in your area, turn on your radio or TV to get the latest emergency information from local authorities. Listen for announcements of a tornado watch or tornado warning."
+const description =
+  'Tornadoes are funnels of rapidly spinning air. Their winds may top 250 miles (400 kilometers) an hour and can clear a pathway a mile (1.6 kilometers) wide and 50 miles (80 kilometers) long.Twisters are born in thunderstorms and are often accompanied by hail. Giant, persistent thunderstorms called supercells spawn the most destructive tornadoes. These violent storms occur around the world, but the United States is a major hotspot with about a thousand tornadoes every year.'
+const description2 =
+  'When there are thunderstorms in your area, turn on your radio or TV to get the latest emergency information from local authorities. Listen for announcements of a tornado watch or tornado warning.'
 class Tornado extends Component {
   render() {
     return (
-      <div className={style.container}>
-    <Link
-      to="/"
-      className="button"
-      style={{ position: 'absolute', top: '160px' }}
-    >
-      <FontAwesomeIcon icon="arrow-left" size="2x" color="#00134d" />
-    </Link>
-    <h1 className={style.title}>TORNADOS</h1>
+      <div style={{ display: 'flex' }}>
+        <div className={style.container}>
+          <Link
+            to="/"
+            className="button"
+            style={{ position: 'absolute', top: '160px' }}
+          >
+            <FontAwesomeIcon
+              style={{ marginTop: '-55px' }}
+              icon="arrow-left"
+              size="2x"
+              color="#00134d"
+            />
+          </Link>
+          <h1 className={style.title}>TORNADOS</h1>
 
-    <div className={style.descript}>
-      <p>{description}</p>
-    </div>
-    <img alt="" src={img1} className={style.img} />
-    <div className={style.descript}>
-    <h1> HOW TO PREPARE</h1>
-      <p>{description2}</p>
-      <p> Here is what we suggest:</p>
-      
-    </div>
+          <div className={style.descript}>
+            <p>{description}</p>
+          </div>
+          <img alt="" src={img1} className={style.img} />
+          <div className={style.descript}>
+            <h1> HOW TO PREPARE</h1>
+            <p>{description2}</p>
+            <p> Here is what we suggest:</p>
+          </div>
+        </div>
         <Checklist title="My Checklist" list_id="tornado" />
       </div>
     )
